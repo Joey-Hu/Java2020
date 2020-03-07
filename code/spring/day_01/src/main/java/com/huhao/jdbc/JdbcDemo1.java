eb26955691c5e92d7e6ea80aab0f7f9d59118847
huhao <1925595006@qq.com>
2020-03-07 12:20:10 +0800

    go fish

---
+++ code/spring/day_01/src/main/java/com/huhao/jdbc/JdbcDemo1.java
@@ -0,0 +1,16 @@
+package com.huhao.jdbc;
+
+/**
+ * @author: huhao
+ * @time: 2020/3/7 12:17
+ * @desc: 程序的耦合
+ */
+public class JdbcDemo1 {
+    // 1. 注册驱动
+    // 2. 获取连接
+    // 3. 获取数据库的预处理对象
+    // 4. 执行sql，得到结果集
+    // 5. 遍历结果集
+    // 6. 释放资源
+
+}
